55845c1542ae84a791b6dadbb3f763bb09bbef2c
Jon Edwards <jkedwards@me.com>
2015-06-26 10:27:49 -0700

    update photo, increase min eq magnitude, adjust styling for Bootstrap logo

--- client/models/usgs.js
+++ client/models/usgs.js
@@ -8,7 +8,7 @@ angular.module('kolohelios')
     var startTime = $window.moment().subtract(14, 'days').format('YYYY-MM-DD');
     var endTime = $window.moment().format('YYYY-MM-DD');
     var timeString = '&starttime=' + startTime + '&endtime' + endTime;
-    $http.get(usgsUrl + timeString + '&latitude=47.6097&longitude=-122.3331&maxradiuskm=402&minmagnitude=2.5')
+    $http.get(usgsUrl + timeString + '&latitude=47.6097&longitude=-122.3331&maxradiuskm=402&minmagnitude=3.0')
     .then(function(response){
       $rootScope.earthquakes = response.data.features.map(function(eq){
         eq.properties.timeAgo = $window.moment(eq.properties.time).fromNow();
